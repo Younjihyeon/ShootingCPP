c8379ecf2830733ab06f8b8ab350581719500046
Younjihyeon <anicoloreando@gmail.com>
2025-07-15 14:29:49 +0900

    PlaterPawn업데이트

---
+++ Source/ShootingCPP/PlayerPawn.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class SHOOTINGCPP_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	//생성자 함수 : 객체화 될 때 어떤 함수보다 먼저 실행된다.
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	// 플레이 버튼을 누를때 혹은, 객체가 생성 될때 1번 
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	// 매 프레임 마다 실행 되는 함수 이다. deltatime 한프레임을 연산하는데 걸린시간
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
