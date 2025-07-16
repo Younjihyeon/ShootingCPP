42ceb4077b5d7973c6399579a6c8c440fcb6f81b
Younjihyeon <anicoloreando@gmail.com>
2025-07-16 13:50:55 +0900

    적생성

---
+++ Source/ShootingCPP/EnemyFactory.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyFactory.h"
+#include "Enemy.h"
+
+// Sets default values
+AEnemyFactory::AEnemyFactory()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemyFactory::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemyFactory::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (CurrentTime >= DelayTime)
+	{
+
+		CurrentTime = 0;
+		//스폰 쿨타임보다 현재 시간이 지났다.
+		GetWorld()->SpawnActor<AEnemy>(
+			EnemyClass, GetActorLocation(),
+			GetActorRotation());
+	}
+	else
+	{
+
+		//스폰 쿨타임보다 현재 시간이 낮다면,
+		//Deltime 1프레임을 연산하는데 걸린시간
+		//currenttime 에 Detatime을 더해준다. 
+		CurrentTime += DeltaTime;
+	}
+}
+
---
+++ Source/ShootingCPP/EnemyFactory.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemyFactory.generated.h"
+
+UCLASS()
+class SHOOTINGCPP_API AEnemyFactory : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemyFactory();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	//스폰 딜레이타임
+	UPROPERTY(EditAnywhere)
+	float DelayTime = 2.0f;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class AEnemy> EnemyClass;
+
+private:
+	//현재 시간
+	float CurrentTime;
+
+};
